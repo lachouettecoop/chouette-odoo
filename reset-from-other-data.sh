085e0d30613857e919d7c0d8d9e16ccf9b111409
root <root@vps207123.ovh.net>
2016-01-24 19:48:10 +0100

    Ajout d'un script basique de synchronisation entre versions

---
+++ reset-from-other-data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATA_TO_USE=$1
+
+echo "Démarrage de la remise à zéro de cette instance d'Odoo."
+echo "... les données seront resynchronisées depuis " $DATA_TO_USE
+
+echo "TODO Ajouter ici une confirmation car ça craint si exécuté par erreur, en expliquant les conséquences tout ça"
+echo ""
+
+echo "Arrêt et suppression de l'instance actuelle"
+docker-compose stop && docker-compose rm -vf
+
+echo "Suppression des données actuelles"
+rm -rf ./data
+
+echo "Récupération des données distantes"
+rsync -avzL $DATA_TO_USE .
+
+echo "Redémarrage de l'instance avec les données à jour"
+docker-compose up -d
+
+echo "... et voilà !"
