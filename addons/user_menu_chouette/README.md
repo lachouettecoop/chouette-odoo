3291447ad2db1b7e9c83a2dd52b1c31df3da3fbd
tyndare <tyndare@wanadoo.fr>
2017-04-27 00:01:25 +0200

    user_menu_chouette: lien «Support» configurable

--- addons/user_menu_chouette/README.md
+++ addons/user_menu_chouette/README.md
@@ -3,6 +3,7 @@ User Menu for La Chouette Coop, Odoo addon
 
 Replace the standard top right UserMenu:  
 
- * Replace "Support" link from Odoo.com/buy to https://gestion.lachouettecoop.fr/projects/assistance/issues/new
+ * Replace "Support" link from Odoo.com/buy to a link configurable in
+        Configuration/General Settings/Menu Support
  * Remove "My Oddo Account" link
 
--- addons/user_menu_chouette/static/src/js/menu.js
+++ addons/user_menu_chouette/static/src/js/menu.js
@@ -1,8 +1,18 @@
+odoo.define('user_menu_chouette.support_chouette', function (require) {
+"use strict";
+
+var Model = require('web.Model');
+var UserMenu = require('web.UserMenu');
+
 // Modify behaviour of addons/web/static/src/js/widgets/user_menu.js
-openerp.user_menu_chouette = function(instance, local) {
-     instance.web.UserMenu.include({
-          on_menu_support_chouette: function () {
-              window.open('https://gestion.lachouettecoop.fr/projects/assistance/issues/new', '_blank');
-          },
-     });
-}
+UserMenu.include({
+    on_menu_support_chouette: function () {
+        //window.open('https://gestion.lachouettecoop.fr/projects/assistance/issues/new', '_blank');
+        var P = new Model('ir.config_parameter');
+        P.call('get_param', ['x_user_menu_support_url']).then(function(url) {
+            window.open(url);
+        });
+    }
+});
+
+});
