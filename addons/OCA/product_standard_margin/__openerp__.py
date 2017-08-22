050e9a7628a77d127245412c562f3f971cd1dc3e
tyndare <tyndare@wanadoo.fr>
2017-08-22 17:09:02 +0200

    Add product_standard_margin addon from OCA
    Add product_standard_margin addon from OCA
    https://gestion.lachouettecoop.fr/issues/1648
    and the dependency addon product_replenishment_cost
    
    https://github.com/OCA/margin-analysis/tree/9.0/product_standard_margin
    https://github.com/OCA/margin-analysis/tree/9.0/product_replenishment_cost

---
+++ addons/OCA/product_standard_margin/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Joel Grand-Guillaume
+#    Copyright 2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Product Margin and Margin Rate',
+    'version': '8.0.2.0.0',
+    'author': 'Camptocamp,GRAP,Odoo Community Association (OCA)',
+    'category': 'Product',
+    'depends': [
+        'product_replenishment_cost',
+        'account',
+    ],
+    'website': 'http://www.camptocamp.com/',
+    'data': [
+        'data/decimal_precision.yml',
+        'views/view.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': False,
+}
